823313b786a8f296dfb3b5b563002476f6afa288
JanorNeto <janorneto@hotmail.com>
2025-10-27 09:27:08 -0300

    correção do login

--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -2,7 +2,7 @@ import { Page, expect, test } from '@playwright/test';
 
 test('usuário obrigatório', async ({ page }) => {
   await login(page, '', 'senha123')
-  await toast(page, 'Informe o seu nome de usuário!s')
+  await toast(page, 'Informe o seu nome de usuário!')
 });
 
 test('senha obrigatória', async ({ page }) => {
